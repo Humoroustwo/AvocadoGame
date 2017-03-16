86f6f62a39a651c0d4fb436c38382124550170b3
Alexander Clarke <alexandermclarke@gmail.com>
2017-03-16 16:20:45 +0000

    added level class, fixed file format, amended template, added test level

--- src/Main.py
+++ src/Main.py
@@ -4,23 +4,27 @@ except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 import time
+import urllib2
+import random
+import math
 
-current_time = time.time()*1000
-last_pressed = current_time+100
+current_time = time.time() * 1000
+last_pressed = current_time + 100
 
-#CONSTANTS
+# CONSTANTS
 WIDTH = 1280
 HEIGHT = 720
 GAMESPEED = 1
-#SPRITE CONSTANTS
+# SPRITE CONSTANTS
 SP_SIZE = [64, 64]
 SP_CENTER = [32, 32]
 
-
-#IMAGES
+# IMAGES
 test_player_image = simplegui._load_local_image("sprite.png")
 test_wall_image = simplegui._load_local_image("block.png")
-#VARIABLES
+
+
+# VARIABLES
 
 
 # ----------------------------------------------------------------------------------------------------------
@@ -41,6 +45,7 @@ class Key:
         if key_code == simplegui.KEY_MAP[self.name]:
             self.pressed = False
 
+
 keys = [Key('a', "1l"),
         Key('d', "1r"),
         Key('w', "1j"),
@@ -48,24 +53,30 @@ keys = [Key('a', "1l"),
         Key('right', "2r"),
         Key('up', "2j")]
 
+
 def key_down(key_code):
     for k in keys:
         k.check_down(key_code)
+
 
 def key_up(key_code):
     for k in keys:
         k.check_up(key_code)
 
+
 # ----------------------------------------------------------------------------------------------------------
 # MOUSE INPUT
 # ----------------------------------------------------------------------------------------------------------
 
-mouse_click_pos = (0,0)
+mouse_click_pos = (0, 0)
 mouse_click = False
+
+
 def mouse_handler(position):
     global mouse_click_pos, mouse_click
     mouse_click = True
     mouse_click_pos = position
+
 
 # ----------------------------------------------------------------------------------------------------------
 # VECTOR CLASS
@@ -73,11 +84,11 @@ def mouse_handler(position):
 
 class Vector:
     def __init__(self):
-        self.x_vec = 0 #Vector, not pos
-        self.y_vec = 0 #Vector, not pos
+        self.x_vec = 0  # Vector, not pos
+        self.y_vec = 0  # Vector, not pos
 
     def accelerate(self, max_speed, acc, dir):
-        self.x_vec += acc*dir
+        self.x_vec += acc * dir
         if self.x_vec > max_speed:
             self.x_vec = max_speed
         elif self.x_vec < -max_speed:
@@ -91,6 +102,7 @@ class Vector:
         if (-acc / 3 < self.x_vec < acc / 3):
             self.x_vec = 0
 
+
 # ----------------------------------------------------------------------------------------------------------
 # ENTITY
 # ----------------------------------------------------------------------------------------------------------
@@ -100,6 +112,20 @@ class Entity(Vector):
         Vector.__init__(self)
         self.x = x
         self.y = y
+
+class Jumper(Entity):
+    def __init__(self):
+        self.todo = 0
+
+class Shooter(Entity):
+    def __init__(self):
+        self.todo = 0
+
+class Soldier(Entity):
+    def __init__(self):
+        self.todo = 0
+
+
 
 # ----------------------------------------------------------------------------------------------------------
 # PLAYER
@@ -112,7 +138,7 @@ class Player:
         self.x_vel = 0
         self.y_vel = 0
         self.width = width
-        self.height =  height
+        self.height = height
         self.max_speed = max_speed
         self.acc = acc
         self.cur_speed = 0
@@ -122,7 +148,7 @@ class Player:
         self.max_jump = max_jump
         self.t_vel = terminal_velocity
 
-    def draw(self, canvas): #Specific to simplegui
+    def draw(self, canvas):  # Specific to simplegui
         global wall_array
         global test_player_image
         canvas.draw_image(test_player_image, SP_CENTER, SP_SIZE, (self.x, self.y), SP_SIZE)
@@ -133,10 +159,9 @@ class Player:
         self.put_back()
 
     def put_back(self):
-        if(self.y > HEIGHT):
+        if (self.y > HEIGHT):
             self.y = HEIGHT / 2
             self.x = WIDTH / 2
-
 
     def collision_x(self, wall_list):
         wall_x = 0
@@ -146,16 +171,16 @@ class Player:
             wall_x = wall.x
             wall_y = wall.y
             wall_w = wall.width
-            if(wall_y +self.height*1.9 >= self.y >= wall_y - self.height):
-                if(wall_x+wall_w > self.x+self.x_vel > wall_x-wall_w):
+            if (wall_y + self.height * 1.9 >= self.y >= wall_y - self.height):
+                if (wall_x + wall_w > self.x + self.x_vel > wall_x - wall_w):
                     self.x_vel = 0
-                    if(self.x<wall_x):
-                        self.x = wall_x-wall_w-1
-                    if(self.x>wall_x):
-                        self.x = wall_x+wall_w+1
+                    if (self.x < wall_x):
+                        self.x = wall_x - wall_w - 1
+                    if (self.x > wall_x):
+                        self.x = wall_x + wall_w + 1
 
     def collision_y(self, wall_list):
-        if(self.y_vel != 0):
+        if (self.y_vel != 0):
             self.in_air = True
 
         wall_x = 0
@@ -176,7 +201,7 @@ class Player:
                     self.y = wall_y - 2 * self.height
                 break
 
-        if ((wall_x - self.width*2) < self.x < (wall_x + self.width*2)) == False:
+        if ((wall_x - self.width * 2) < self.x < (wall_x + self.width * 2)) == False:
             self.on_ground = False
 
         for wall in wall_list:
@@ -185,42 +210,41 @@ class Player:
             wall_w = wall.width
             wall.h = wall.height
 
-            if (wall_x - self.width*2 <= self.x <= wall_x + self.width*2
-                and wall_y < self.y < wall_y + self.height*2.1):
+            if (wall_x - self.width * 2 <= self.x <= wall_x + self.width * 2
+                and wall_y < self.y < wall_y + self.height * 2.1):
                 self.y_vel = 0
 
     def gravity(self):
         if (not self.on_ground):
             self.y_vel += self.grav
-            if(self.y_vel > self.t_vel): #Terminal velocity
+            if (self.y_vel > self.t_vel):  # Terminal velocity
                 self.y_vel = self.t_vel
-            self.y += self.y_vel*GAMESPEED
+            self.y += self.y_vel * GAMESPEED
 
     def accelerate(self, move):
-        self.x_vel += self.acc*move
+        self.x_vel += self.acc * move
         self.collision_x(wall_array)
         if self.x_vel > self.max_speed:
             self.x_vel = self.max_speed
         elif self.x_vel < -self.max_speed:
             self.x_vel = -self.max_speed
 
-
     def decelerate(self):
         if self.x_vel < 0:
             self.x_vel += self.acc / 2
         elif self.x_vel > 0:
             self.x_vel += -self.acc / 2
-        if(-self.acc/3 < self.x_vel < self.acc/3):
+        if (-self.acc / 3 < self.x_vel < self.acc / 3):
             self.x_vel = 0
         self.collision_x(wall_array)
 
     def jump(self):
-        if(self.on_ground):
+        if (self.on_ground):
             self.in_air = True
             self.on_ground = False
             self.y_vel -= self.max_jump
 
-    def control(self): #Specific to simplegui
+    def control(self):  # Specific to simplegui
         last_pressed = 0
         for k in keys:
             if k.pressed:
@@ -237,15 +261,15 @@ class Player:
                 if (last_pressed + 100 < time.time() * 1000):
                     self.decelerate()
 
-            self.x += self.x_vel*GAMESPEED
-            self.y += self.y_vel*GAMESPEED
+            self.x += self.x_vel * GAMESPEED
+            self.y += self.y_vel * GAMESPEED
+
 
 # ----------------------------------------------------------------------------------------------------------
 # GENERIC WALLS
 # ----------------------------------------------------------------------------------------------------------
 
 class Wall():
-
     def __init__(self, x, y, width, height, sprite):
         self.x = x
         self.y = y
@@ -253,37 +277,38 @@ class Wall():
         self.height = height
         self.sprite = sprite
 
-    def draw(self, canvas): #Specific to simplegui
+    def draw(self, canvas):  # Specific to simplegui
         canvas.draw_image(self.sprite,
-                          (self.width/2, self.height/2),
+                          (self.width / 2, self.height / 2),
                           (self.width, self.height),
                           (self.x, self.y),
                           (self.width, self.height))
+
 
 # ----------------------------------------------------------------------------------------------------------
 # ENTITIES
 # ----------------------------------------------------------------------------------------------------------
 
-player_one = Player(1, WIDTH/2, HEIGHT/2, 32, 32, 0.2, 1.5, 5, 6)
+player_one = Player(1, WIDTH / 2, HEIGHT / 2, 32, 32, 0.2, 1.5, 5, 6)
 
 wall_array = []
-for i in range(0, int(WIDTH/64)+1):
-    wall_array.append(Wall(i*64, HEIGHT, 64, 64, test_wall_image))
+for i in range(0, int(WIDTH / 64) + 1):
+    wall_array.append(Wall(i * 64, HEIGHT, 64, 64, test_wall_image))
 
-for i in range(0, int(WIDTH/64)+1):
-    wall_array.append(Wall(i*64, 0, 64, 64, test_wall_image))
+for i in range(0, int(WIDTH / 64) + 1):
+    wall_array.append(Wall(i * 64, 0, 64, 64, test_wall_image))
 
-for i in range(0, int(HEIGHT/64)+1):
-    wall_array.append(Wall(0, i*64, 64, 64, test_wall_image))
+for i in range(0, int(HEIGHT / 64) + 1):
+    wall_array.append(Wall(0, i * 64, 64, 64, test_wall_image))
 
-for i in range(0, int(HEIGHT/64)+1):
-    wall_array.append(Wall(WIDTH, i*64, 64, 64, test_wall_image))
-
-for i in range(0, 6):
-    wall_array.append(Wall(i*64, HEIGHT/2, 64, 64, test_wall_image))
+for i in range(0, int(HEIGHT / 64) + 1):
+    wall_array.append(Wall(WIDTH, i * 64, 64, 64, test_wall_image))
 
 for i in range(0, 6):
-    wall_array.append(Wall(WIDTH-(i*64), HEIGHT/2, 64, 64, test_wall_image))
+    wall_array.append(Wall(i * 64, HEIGHT / 2, 64, 64, test_wall_image))
+
+for i in range(0, 6):
+    wall_array.append(Wall(WIDTH - (i * 64), HEIGHT / 2, 64, 64, test_wall_image))
 
 
 # ----------------------------------------------------------------------------------------------------------
@@ -291,28 +316,107 @@ for i in range(0, 6):
 # ----------------------------------------------------------------------------------------------------------
 
 class Game:
-
     def __init__(self):
         self.game_speed = 1
 
-    def draw(self, canvas): #Specific to simplegui
+    def draw(self, canvas):  # Specific to simplegui
         player_one.draw(canvas)
         for wall in wall_array:
             wall.draw(canvas)
+
+
+# ----------------------------------------------------------------------------------------------------------
+# Level
+# ----------------------------------------------------------------------------------------------------------
+
+#name:Testing Level
+#background_image:
+#wall_image:
+#walls:xpos,ypos,width,height|xpos,ypos,width,height|xpos,ypos,width,height
+#min_enemies:10
+#max_enemies:20
+#min_time_between_spawn:
+#max_time_between_spawn:
+#proportion_shooters: 0.2
+#proportion_bouncers: 0.5
+#proportion_soldiers: 0.3
+
+
+
+
+class Level:
+    def __init__(self, file_path):
+        # Level Metadata
+        raw_file = urllib2.urlopen(simplegui.file2url("./data/levels/" + file_path)).readlines()
+        self.level_name = self.get_level_file_field("name", raw_file)
+
+        # Loading textures
+        self.background = simplegui.load_image(self.get_level_file_field("background_image", raw_file))
+        self.wall_texture = simplegui.load_image(self.get_level_file_field("wall_image", raw_file))
+
+        # Enemy variables
+        self.min_enemy_count = self.get_level_file_field("min_enemies", raw_file)
+        self.max_enemy_count = self.get_level_file_field("max_enemies", raw_file)
+        self.min_time_between_spawn = self.get_level_file_field("min_time_between_spawn", raw_file)
+        self.max_time_between_spawn = self.get_level_file_field("max_time_between_spawn", raw_file)
+
+        # Generating enemy queue
+
+        self.proportion_shooters = self.get_level_file_field("proportion_shooters", raw_file)
+        self.proportion_bouncers = self.get_level_file_field("proportion_bouncers", raw_file)
+        self.proportion_soldiers = self.get_level_file_field("proportion_soldiers", raw_file)
+        self.enemy_queue = None
+        self.generate_enemies_queue()
+
+
+    def generate_enemies_queue(self):
+        rand = random.seed()
+        enemy_count = math.ceil((self.max_enemy_count - self.min_enemy_count)) + self.min_enemy_count
+
+        enemies = []
+        # Filling list initially (for each enemy type)
+        for _ in range(round(self.proportion_shooters*enemy_count)):
+            enemies.append(Shooter())
+        for _ in range(round(self.proportion_shooters*enemy_count)):
+            enemies.append(Jumper())
+        for _ in range(round(self.proportion_shooters*enemy_count)):
+            enemies.append(Soldier())
+
+        # Randomising List
+        for x in enemies:
+
+            j = round(random.random()*enemies.count())
+            swap = enemies[x]
+            enemies[x] = enemies[j]
+            enemies[x] = swap
+
+        self.enemy_queue = enemies
+
+    def get_level_file_field(self, field_name, file_lines):
+        for i in file_lines:
+            line = i.split(":")
+            if line[0] == field_name:
+                return line[1]
+        print ("Warning: Field \"" + field_name + + "\" not found!")
+
 
 # ----------------------------------------------------------------------------------------------------------
 # MAIN DISPLAY
 # ----------------------------------------------------------------------------------------------------------
 new_game = Game()
+
+
 def display(canvas):
     new_game.draw(canvas)
+
 
 # ----------------------------------------------------------------------------------------------------------
 # SIMPLE GUI FRAME
 # ----------------------------------------------------------------------------------------------------------
 
-frame = simplegui.create_frame('tmp', WIDTH, HEIGHT)
 
+
+frame = simplegui.create_frame('tmp', WIDTH, HEIGHT)
 frame.set_canvas_background('rgb(12,50,120)')
 frame._display_fps_average = True
 frame.set_draw_handler(display)
@@ -321,4 +425,3 @@ frame.set_keyup_handler(key_up)
 frame.set_mouseclick_handler(mouse_handler)
 
 frame.start()
-
